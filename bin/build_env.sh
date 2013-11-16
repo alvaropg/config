cf133169ad10993862e9c9cc05bcdd835499d8c4
Álvaro Peña <alvaropg@gmail.com>
2013-11-16 13:29:06 +0100

    A little script to export my personal build environment variables

---
+++ bin/build_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BUILD_PATH=/opt/build
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PATH/lib/pkgconfig
+export XDG_DATA_DIRS=$XDG_DATA_DIRS:$BUILD_PATH/share:/usr/share
+export GI_TYPELIB_PATH=$GI_TYPELIB_PATH:$BUILD_PATH/lib/girepository-1.0
