3d705a1fab0330777563a188d38509bf55303919
Álvaro Peña <alvaropg@gmail.com>
2016-03-25 16:33:05 +0100

    Added some XDG path

--- bin/build_env.sh
+++ bin/build_env.sh
@@ -3,5 +3,5 @@
 BUILD_PATH=/opt/build
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PATH/lib/pkgconfig
-export XDG_DATA_DIRS=$XDG_DATA_DIRS:$BUILD_PATH/share:/usr/share
+export XDG_DATA_DIRS=$XDG_DATA_DIRS:$BUILD_PATH/share:/usr/share:/usr/local/share
 export GI_TYPELIB_PATH=$GI_TYPELIB_PATH:$BUILD_PATH/lib/girepository-1.0
