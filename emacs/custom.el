aa40e8d015ccf836900d41392726700ce45ff2b3
Álvaro Peña <alvaropg@gmail.com>
2020-11-09 20:04:22 +0100

    [emacs] Added custom.el file

---
+++ emacs/custom.el
@@ -0,0 +1,37 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(case-fold-search t)
+ '(column-number-mode t)
+ '(custom-safe-themes
+   '("24714e2cb4a9d6ec1335de295966906474fdb668429549416ed8636196cb1441" "eb122e1df607ee9364c2dfb118ae4715a49f1a9e070b9d2eb033f1cefd50a908" default))
+ '(delete-selection-mode t)
+ '(fset 'yes-or-no-p t)
+ '(global-linum-mode t)
+ '(inhibit-startup-echo-area-message t)
+ '(inhibit-startup-screen t)
+ '(initial-scratch-message nil)
+ '(menu-bar-mode nil)
+ '(package-selected-packages
+   '(nyan-mode dracula-theme magit auto-package-update use-package))
+ '(require-final-newline t)
+ '(scroll-bar-mode nil)
+ '(scroll-conservatively 100000)
+ '(scroll-margin 5)
+ '(scroll-preserve-screen-position t)
+ '(scroll-step 1)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(transient-mark-mode t)
+ '(truncate-lines t)
+ '(user-full-name "Álvaro Peña")
+ '(user-mail-address "alvaropg@gmail.com")
+ '(visible-bell t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
