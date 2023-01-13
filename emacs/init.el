82fd17ae5b8de62e25bf41c609169ef4ce2f99b9
Álvaro Peña <alvaropg@gmail.com>
2023-01-13 22:27:08 +0100

    [emacs] Removed which-key integration

--- emacs/init.el
+++ emacs/init.el
@@ -88,9 +88,7 @@
   (setq lsp-keymap-prefix "C-c l")
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
          (c-mode . lsp)
-	 (c++-mode . lsp)
-         ;; if you want which-key integration
-         (lsp-mode . lsp-enable-which-key-integration))
+	 (c++-mode . lsp))
   :commands lsp)
 ;; (use-package lsp-ui :commands lsp-ui-mode :after lsp-mode)
 ;; Because I'm an ivy user
